6a83bded9eb0438cb9b079cbf0d00b29ead01653
Yuta Yamada <cokesboy@gmail.com>
2015-01-15 00:09:58 -0800

    Refactor

--- rc.lua
+++ rc.lua
@@ -696,29 +696,28 @@ end
 -- }}}
 -- }}}
 
-x = 0
-
--- setup the timer
-mytimer = timer { timeout = x }
-mytimer:connect_signal("timeout", function()
-
-  -- tell awsetbg to randomly choose a wallpaper from your wallpaper directory
-  if file_exists(wallpaper_dir) and whereis_app('feh') then
-    os.execute(wallpaper_cmd)
-  end
-  -- stop the timer (we don't need multiple instances running at the same time)
-  mytimer:stop()
-
-  -- define the interval in which the next wallpaper change should occur in seconds
-  -- (in this case anytime between 10 and 20 minutes)
-  x = math.random( 600, 1200)
-
-  --restart the timer
-  mytimer.timeout = x
+-----------------------
+-- change background --
+-----------------------
+if whereis_app('feh') then
+  mytimer = timer { timeout = 0 }
+  mytimer:connect_signal("timeout",
+                         function()
+                           -- tell awsetbg to randomly choose a wallpaper from your wallpaper directory
+                           if file_exists(wallpaper_dir) then
+                             os.execute(wallpaper_cmd)
+                           end
+                           -- stop the timer (we don't need multiple instances running at the same time)
+                           mytimer:stop()
+                           -- define the interval in which the next wallpaper change should occur in seconds
+                           -- (in this case anytime between 10 and 20 minutes)
+                           x = math.random(600, 1200)
+                           --restart the timer
+                           mytimer.timeout = x
+                           mytimer:start()
+                         end)
+  -- initial start when rc.lua is first run
   mytimer:start()
-end)
-
--- initial start when rc.lua is first run
-mytimer:start()
+end
 
 require_safe('autorun')
